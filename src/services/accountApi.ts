73b4fe55f1e61c70289440367d1fd3ecaac34cbb
Kaden Rice <kadenrice13@gmail.com>
2023-12-07 11:14:48 -0800

    error core

--- src/services/accountApi.ts
+++ src/services/accountApi.ts
@@ -3,9 +3,13 @@ import Account from "../models/Account";
 
 const baseUrl: string = import.meta.env.VITE_APP_BASE_URL ?? "NOT FOUND";
 
-export const getAccountById = async (uid: string): Promise<Account> => {
-  return (await axios.get(`${baseUrl}/accounts/${encodeURIComponent(uid)}`))
-    .data;
+export const getAccountById = async (uid: string): Promise<Account | void> => {
+  try {
+    return (await axios.get(`${baseUrl}/accounts/${encodeURIComponent(uid)}`))
+      .data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const createNewAccount = async (account: Account): Promise<Account> => {
